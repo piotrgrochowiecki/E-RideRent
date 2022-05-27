a43280f9f501a0eb08b6359c48fab2514f156b27
Piotr Grochowiecki <42697061+piotrgrochowiecki@users.noreply.github.com>
2022-05-27 22:28:47 +0200

    Added ReviewEntity

---
+++ src/main/java/com/piotgrochowiecki/eriderent/model/ReviewEntity.java
@@ -0,0 +1,22 @@
+package com.piotgrochowiecki.eriderent.model;
+
+import lombok.*;
+
+import javax.persistence.*;
+import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.Size;
+
+@Entity
+@Table(name = "reviews")
+@Setter @Getter @ToString @Builder
+@NoArgsConstructor @AllArgsConstructor
+public class ReviewEntity {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @NotBlank @Size(max = 3000)
+    private String review;
+
+}
