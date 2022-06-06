b477ca05d113418142db1de8529e11ec3169ce14
Piotr Grochowiecki <42697061+piotrgrochowiecki@users.noreply.github.com>
2022-06-06 02:44:04 +0200

    Annotation @Repository added to repositories

--- src/main/java/com/piotgrochowiecki/eriderent/repository/CustomerRepository.java
+++ src/main/java/com/piotgrochowiecki/eriderent/repository/CustomerRepository.java
@@ -2,7 +2,9 @@ package com.piotgrochowiecki.eriderent.repository;
 
 import com.piotgrochowiecki.eriderent.model.CustomerEntity;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
+@Repository
 public interface CustomerRepository extends JpaRepository<CustomerEntity, Long> {
 
 }
--- src/main/java/com/piotgrochowiecki/eriderent/repository/ReservationPeriodRepository.java
+++ src/main/java/com/piotgrochowiecki/eriderent/repository/ReservationPeriodRepository.java
@@ -1,6 +1,8 @@
 package com.piotgrochowiecki.eriderent.repository;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
+@Repository
 public interface ReservationPeriodRepository extends JpaRepository<ReservationPeriodRepository, Long> {
 }
