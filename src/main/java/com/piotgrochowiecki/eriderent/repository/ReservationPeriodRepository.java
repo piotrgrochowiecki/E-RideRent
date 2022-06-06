90a4352bbcb96a152e9be5de4780e3c50a9f3439
Piotr Grochowiecki <42697061+piotrgrochowiecki@users.noreply.github.com>
2022-06-06 20:22:43 +0200

    Typo fixed

--- src/main/java/com/piotgrochowiecki/eriderent/repository/ReservationPeriodRepository.java
+++ src/main/java/com/piotgrochowiecki/eriderent/repository/ReservationPeriodRepository.java
@@ -1,8 +1,9 @@
 package com.piotgrochowiecki.eriderent.repository;
 
+import com.piotgrochowiecki.eriderent.model.ReservationPeriodEntity;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 @Repository
-public interface ReservationPeriodRepository extends JpaRepository<ReservationPeriodRepository, Long> {
+public interface ReservationPeriodRepository extends JpaRepository<ReservationPeriodEntity, Long> {
 }
