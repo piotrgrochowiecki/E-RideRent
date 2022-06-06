62e9f97e5d389ae4534b8072447e74589d4449da
Piotr Grochowiecki <42697061+piotrgrochowiecki@users.noreply.github.com>
2022-06-06 02:14:33 +0200

    Home controller moved

--- src/main/java/com/piotgrochowiecki/eriderent/controller/mvc/HomeController.java
+++ src/main/java/com/piotgrochowiecki/eriderent/controller/mvc/HomeController.java
@@ -1,4 +1,4 @@
-package com.piotgrochowiecki.eriderent.controller;
+package com.piotgrochowiecki.eriderent.controller.mvc;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
