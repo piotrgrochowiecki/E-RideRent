498ccfd601b0a7e1f7a24c679d40c2d26a8801c1
Piotr Grochowiecki <42697061+piotrgrochowiecki@users.noreply.github.com>
2023-01-13 20:42:20 +0100

    Injection through constructor fixed

--- src/main/java/com/piotgrochowiecki/eriderent/service/RoleService.java
+++ src/main/java/com/piotgrochowiecki/eriderent/service/RoleService.java
@@ -3,16 +3,22 @@ package com.piotgrochowiecki.eriderent.service;
 import com.piotgrochowiecki.eriderent.model.Role;
 import com.piotgrochowiecki.eriderent.model.User;
 import com.piotgrochowiecki.eriderent.repository.RoleRepository;
-import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
 
-@Service
-@RequiredArgsConstructor
+@Service("RoleService")
+@Slf4j
 public class RoleService implements RoleServiceInterface {
 
     private final RoleRepository roleRepository;
+
+    @Autowired
+    public RoleService(RoleRepository roleRepository) {
+        this.roleRepository = roleRepository;
+    }
 
     @Override
     public List<Role> findAll() {
