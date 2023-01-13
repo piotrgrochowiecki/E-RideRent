e69536963be1570b036305c6c6981a513c6526b0
Piotr Grochowiecki <42697061+piotrgrochowiecki@users.noreply.github.com>
2023-01-13 20:38:38 +0100

    Injection through constructor fixed

--- src/main/java/com/piotgrochowiecki/eriderent/service/PositionService.java
+++ src/main/java/com/piotgrochowiecki/eriderent/service/PositionService.java
@@ -2,16 +2,22 @@ package com.piotgrochowiecki.eriderent.service;
 
 import com.piotgrochowiecki.eriderent.model.Position;
 import com.piotgrochowiecki.eriderent.repository.PositionRepository;
-import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import java.util.Optional;
 
-@Service
-@RequiredArgsConstructor
+@Service("PositionService")
+@Slf4j
 public class PositionService implements PositionServiceInterface {
 
     private final PositionRepository positionRepository;
+
+    @Autowired
+    public PositionService(PositionRepository positionRepository) {
+        this.positionRepository = positionRepository;
+    }
 
     @Override
     public Optional<Position> findCarsLatestPosition(long carId) {
