4589baceea1af3b1239163e60e07169393fde93e
Piotr Grochowiecki <42697061+piotrgrochowiecki@users.noreply.github.com>
2023-02-17 15:26:45 +0100

    NoUserFoundException added

---
+++ src/main/java/com/piotgrochowiecki/eriderent/exception/NoUserFoundException.java
@@ -0,0 +1,7 @@
+package com.piotgrochowiecki.eriderent.exception;
+
+public class NoUserFoundException extends Exception {
+    public NoUserFoundException(String message) {
+        super(message);
+    }
+}
