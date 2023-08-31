0697ed0777870aafadfcca67dc9025ef47cd0d68
Piotr Grochowiecki <42697061+piotrgrochowiecki@users.noreply.github.com>
2023-08-31 07:21:19 +0200

    migration from local db to docker container

---
+++ src/main/resources/eriderentdb.sql
@@ -0,0 +1,146 @@
+-- MySQL dump 10.13  Distrib 8.0.29, for Win64 (x86_64)
+--
+-- Host: localhost    Database: eriderentdb
+-- ------------------------------------------------------
+-- Server version	8.0.29
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!50503 SET NAMES utf8mb4 */;
+/*!40103 SET @OLD_TIME_ZONE=@@TIME_ZONE */;
+/*!40103 SET TIME_ZONE='+00:00' */;
+/*!40014 SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0 */;
+/*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
+/*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
+/*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
+
+--
+-- Table structure for table `cars`
+--
+
+DROP TABLE IF EXISTS `cars`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `cars` (
+  `id` bigint NOT NULL AUTO_INCREMENT,
+  `acceleration_sec` double DEFAULT NULL,
+  `brand` varchar(255) COLLATE utf8mb4_unicode_ci DEFAULT NULL,
+  `fast_charge_kmh` int DEFAULT NULL,
+  `model` varchar(255) COLLATE utf8mb4_unicode_ci DEFAULT NULL,
+  `power_train` varchar(255) COLLATE utf8mb4_unicode_ci DEFAULT NULL,
+  `range_km` int DEFAULT NULL,
+  `top_speed_kmh` int DEFAULT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=27 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `cars`
+--
+
+LOCK TABLES `cars` WRITE;
+/*!40000 ALTER TABLE `cars` DISABLE KEYS */;
+INSERT INTO `cars` VALUES (2,5,'Suzuki',5,'e-Swift','FWD',325,160),(4,3.2,'Audi',54,'eTron','AWD',453,199),(5,4.5,'Tesla',546,'Model S','AWD',348,232),(7,6.4,'Honda',333,'e','AWD',443,201),(10,4.1,'Mercedes',546,'EQC','AWD',546,189),(17,3,'Tesla',300,'Model Y','FWD',400,290),(20,5,'testBrand',35,'testModel','FWD',300,200),(23,5.4,'testBrandEdited234',34,'testModelEdited234','AWD',304,204);
+/*!40000 ALTER TABLE `cars` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `position`
+--
+
+DROP TABLE IF EXISTS `position`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `position` (
+  `id` bigint NOT NULL AUTO_INCREMENT,
+  `latitude` double NOT NULL,
+  `longitude` double NOT NULL,
+  `time` datetime(6) NOT NULL,
+  `car_id` bigint DEFAULT NULL,
+  PRIMARY KEY (`id`),
+  KEY `FKd0d4r76ox1n48ru6jfd9ddltg` (`car_id`),
+  CONSTRAINT `FKd0d4r76ox1n48ru6jfd9ddltg` FOREIGN KEY (`car_id`) REFERENCES `cars` (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=4 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `position`
+--
+
+LOCK TABLES `position` WRITE;
+/*!40000 ALTER TABLE `position` DISABLE KEYS */;
+INSERT INTO `position` VALUES (1,51.107883,17.038538,'2022-07-01 19:58:55.000000',17),(2,52.237049,21.017532,'2022-07-05 12:01:06.000000',17),(3,54.372158,18.638306,'2022-07-04 11:02:16.000000',17);
+/*!40000 ALTER TABLE `position` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `reservations`
+--
+
+DROP TABLE IF EXISTS `reservations`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `reservations` (
+  `id` bigint NOT NULL AUTO_INCREMENT,
+  `end_date` date DEFAULT NULL,
+  `start_date` date DEFAULT NULL,
+  `car_id` bigint DEFAULT NULL,
+  `user_id` bigint DEFAULT NULL,
+  PRIMARY KEY (`id`),
+  KEY `FKkoxuu4vp8ex6mcc642sa11iyc` (`car_id`),
+  KEY `FKb5g9io5h54iwl2inkno50ppln` (`user_id`),
+  CONSTRAINT `FKb5g9io5h54iwl2inkno50ppln` FOREIGN KEY (`user_id`) REFERENCES `users` (`id`),
+  CONSTRAINT `FKkoxuu4vp8ex6mcc642sa11iyc` FOREIGN KEY (`car_id`) REFERENCES `cars` (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=28 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `reservations`
+--
+
+LOCK TABLES `reservations` WRITE;
+/*!40000 ALTER TABLE `reservations` DISABLE KEYS */;
+INSERT INTO `reservations` VALUES (2,'2023-02-05','2023-01-31',2,6),(5,'2023-03-08','2023-03-01',4,6),(6,'2023-03-02','2023-02-23',17,6),(7,'2023-03-08','2023-03-01',7,6),(8,'2023-03-02','2023-02-23',20,6),(9,'2023-02-28','2023-02-23',23,6),(21,'2023-06-28','2023-06-14',5,6),(25,'2023-06-29','2023-06-15',4,6),(26,'2023-06-29','2023-06-15',7,6),(27,'2023-06-27','2023-07-19',17,6);
+/*!40000 ALTER TABLE `reservations` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `users`
+--
+
+DROP TABLE IF EXISTS `users`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `users` (
+  `id` bigint NOT NULL AUTO_INCREMENT,
+  `driving_license_issue_date` date DEFAULT NULL,
+  `email` varchar(255) COLLATE utf8mb4_unicode_ci DEFAULT NULL,
+  `first_name` varchar(255) COLLATE utf8mb4_unicode_ci DEFAULT NULL,
+  `last_name` varchar(255) COLLATE utf8mb4_unicode_ci DEFAULT NULL,
+  `password` varchar(255) COLLATE utf8mb4_unicode_ci DEFAULT NULL,
+  `role` varchar(255) COLLATE utf8mb4_unicode_ci NOT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=25 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `users`
+--
+
+LOCK TABLES `users` WRITE;
+/*!40000 ALTER TABLE `users` DISABLE KEYS */;
+INSERT INTO `users` VALUES (5,'2022-07-01','admin@test.com','admin','admin','$2a$10$HyXYoKdRPoMxv.ZZJeX9Lu3AJwy2CcoMjlT/tlSj1USDaH5/Vsi5S','ADMIN'),(6,'2022-07-01','user@test.com','user','user','$2a$10$FsoWm.4cnP3cvohiQAX6Aud/lxXwFDLVJowRzeGBHiIqdtV0ydS3e','USER'),(7,'2022-11-09','jan.kowalski@wp.pl','Jan','Kowalski','$2a$10$Lst1HukzCS5YGbtlBUrNAON1/Ts38Dwlj22ebM.x6fyZ7SQtK0Z1i','USER'),(10,'2022-11-01','jan.kowalsk1212i@wp.pl','Jan','Kowalski','$2a$10$DxwQyCIvFXiqXFgFivdpBOk.PD62Xsf2sDL40re6AqOwjM7.kzkJi','USER'),(11,'2022-11-08','jan.kowalski211111@wp.pl','Jan','Kowalski','$2a$10$N4izyTWzAefCCFbQOlaUBOe9nUICOQN/Csu2eBwGaaOZyboG7xmeK','USER');
+/*!40000 ALTER TABLE `users` ENABLE KEYS */;
+UNLOCK TABLES;
+/*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
+
+/*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
+/*!40014 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS */;
+/*!40014 SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS */;
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
+/*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
+
+-- Dump completed on 2023-08-30 11:43:00
