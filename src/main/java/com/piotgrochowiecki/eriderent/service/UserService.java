c7ec7a40b9bdf32a4971429d886a34b2874c3e36
piotrgrochowiecki <piotrgrochowiecki@gmail.com>
2023-09-28 15:36:47 +0200

    added Javadoc

--- src/main/java/com/piotgrochowiecki/eriderent/service/UserService.java
+++ src/main/java/com/piotgrochowiecki/eriderent/service/UserService.java
@@ -15,6 +15,9 @@ import org.springframework.transaction.annotation.Transactional;
 import java.util.List;
 import java.util.stream.Collectors;
 
+/**
+ * User service
+ */
 @Service("userService")
 @Transactional
 @CommonsLog
@@ -23,12 +26,24 @@ public class UserService implements UserServiceInterface {
     private final UserRepository userRepository;
     private final PasswordEncoder passwordEncoder;
 
+    /**
+     * Constructor
+     *
+     * @param userRepository user repository
+     * @param passwordEncoder password encoder
+     */
     @Autowired
     public UserService(UserRepository userRepository, PasswordEncoder passwordEncoder) {
         this.userRepository = userRepository;
         this.passwordEncoder = passwordEncoder;
     }
 
+    /**
+     * Takes <code>UserRegisterRequestDto</code> type of object, maps it to <code>User</code> entity object and passes it into repository.
+     *
+     * @param userDto UserRegisterRequestDto
+     * @throws EmailAlreadyExistsException when user with given email address already exists in database
+     */
     @Override
     public void registerNewAccount(UserRegisterRequestDto userDto) throws EmailAlreadyExistsException {
         if (emailExists(userDto.getEmail())) {
@@ -45,12 +60,24 @@ public class UserService implements UserServiceInterface {
         return userRepository.findByEmail(email).isPresent();
     }
 
+    /**
+     * Returns <code>UserResponseDto</code> based on id of existing user entity
+     *
+     * @param id id of requested car
+     * @return UserResponseDto object mapped from car entity
+     * @throws NoUserFoundException when no user with given id has been found
+     */
     @Override
     public UserResponseDto getById(Long id) throws NoUserFoundException {
         return userRepository.findById(id).map(UserResponseDto::map)
                 .orElseThrow(() -> new NoUserFoundException("No user with id " + id + " has been found."));
     }
 
+    /**
+     * Updates name and last name for <code>user</code> for which id already exists in database
+     *
+     * @param userResponseDto userResponseDto
+     */
     @Override
     public void update(UserResponseDto userResponseDto) {
         User user = User.builder()
@@ -61,17 +88,35 @@ public class UserService implements UserServiceInterface {
         userRepository.save(user);
     }
 
+    /**
+     * Deletes a <code>user</code> entity with given id
+     *
+     * @param id id of user to be deleted
+     */
     @Override
     public void deleteById(Long id) {
         userRepository.deleteById(id);
     }
 
+
+    /**
+     * Returns <code>UserResponseDto</code> object of analogous <code>user</code> entity based on provided
+     *
+     * @param email email address of requested user
+     * @return UserResponseDto object of analogous user entity
+     * @throws NoUserFoundException when user with given email address has been found
+     */
     @Override
     public UserResponseDto getByEmail(String email) throws NoUserFoundException {
         return userRepository.findByEmail(email).map(UserResponseDto::map)
                 .orElseThrow(() -> new NoUserFoundException("No user with email " + email + " has been found."));
     }
 
+    /**
+     * Returns list of all saved users (as <code>UserResponseDto</code> objects)
+     *
+     * @return list of all saved users (as <code>UserResponseDto</code> objects)
+     */
     @Override
     public List<UserResponseDto> getAll() {
         return userRepository.findAll().stream()
